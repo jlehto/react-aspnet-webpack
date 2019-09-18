98df4a5dff508cabf1006d979cbfb639a67f009d
jlehto <lehto.jarkko@gmail.com>
2019-09-18 14:06:04 +0300

    added react

---
+++ clientApp/Client.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Hello from "./Hello"
+
+ReactDOM.render(
+    <Hello name="World"/>,
+    document.getElementById("app")
+
+)
---
+++ clientApp/Hello.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class Hello extends Component {
+    render() {
+        return (<div> HelloO! </div>)
+    }
+}
+
+Hello.propTypes = {}
+Hello.defaultProps = {}
+export default Hello;
